b69f9e2b21a2521fd99a4065c9d4ea57604f5559
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-07-02 15:23:57 +0200

    added Card component(?)

--- 2-Expense tracker/expensesApp/src/components/ExpenseDateJS.js
+++ 2-Expense tracker/expensesApp/src/components/ExpenseDateJS.js
@@ -1,14 +1,16 @@
+import Card from './Card';
+
 function ExpenseDateJS(props) {
   const year = props.date.toLocaleString('en-US', { year: 'numeric' });
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
 
   return (
-    <div className='option'>
+    <Card className='option'>
       <div>{year}-</div>
       <div>{month}-</div>
       <div>{day}</div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseDateJS;
--- 2-Expense tracker/expensesApp/src/components/ExpenseItemJS.js
+++ 2-Expense tracker/expensesApp/src/components/ExpenseItemJS.js
@@ -1,12 +1,13 @@
 import ExpenseDateJS from './ExpenseDateJS';
+import Card from './Card';
 import './ExpenseItemCSS.css';
 function ExpenseItemJS(props) {
   return (
     <div>
       <div className='wrap'>
-        <div className='option'>{props.title}</div>
-        <div className='option'>{props.id}</div>
-        <div className='option'>{props.amount}</div>
+        <Card className='option'>{props.title}</Card>
+        <Card className='option'>{props.id}</Card>
+        <Card className='option'>{props.amount}</Card>
         <ExpenseDateJS date={props.date} />
       </div>
     </div>
