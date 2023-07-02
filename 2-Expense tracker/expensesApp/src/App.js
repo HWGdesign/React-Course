2daa0bffc64540ce3cf53960e24167b5fd0fbbd9
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-07-02 13:14:11 +0200

    props

--- 2-Expense tracker/expensesApp/src/App.js
+++ 2-Expense tracker/expensesApp/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { id: 1, title: 'paper', amount: 100 },
+    { id: 2, title: 'duck', amount: 200 },
+    { id: 3, title: 'rock', amount: 300 },
+  ];
+
   return (
     <div>
-      <h1>Hi!</h1>
-      <p>React is super cool</p>
-      <ExpenseItem />
+      <h1>React project!</h1>
+      <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} />
+      <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} />
+      <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} />
     </div>
   );
 }
--- 2-Expense tracker/expensesApp/src/components/ExpenseItem.js
+++ 2-Expense tracker/expensesApp/src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
-function ExpenseItem() {
-  return <button>Item</button>;
+import './ExpenseItem.css';
+function ExpenseItem(props) {
+  return (
+    <div>
+      <div className='wrap'>
+        <div className='option'>{props.title}</div>
+        <div className='option'>{props.id}</div>
+        <div className='option'>{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
